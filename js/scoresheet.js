0e18c90c950d60108167aa50edf12f6f27690a1a
Pablo Santa Cruz <pablo@roshka.com.py>
2014-05-15 13:09:31 -0400

    Se arregló el cálculo del puntaje y el máximo (con el correspondiente máximo/máximo). Se ignoraron los archivos de backup del VIM. Se sacaron las cabeceras innecesarias en el UI.

--- js/scoresheet.js
+++ js/scoresheet.js
@@ -3,15 +3,18 @@
   // private members
   var first_shot_bonus = 5;
   var actual_scores = {
+    '_4': 4,
+    '_5': 5,
+    '_6': 6,
     '_straight': 20,
     '_fullhouse': 30,
     '_four_of_a_kind': 40,
     '_yahtzee': 50
   };
   var max_scores = {
-    '_4': 16,
-    '_5': 20,
-    '_6': 30,
+    '_4': actual_scores['_4'] * 5,
+    '_5': actual_scores['_5'] * 5,
+    '_6': actual_scores['_6'] * 5,
     '_straight': actual_scores['_straight'] + first_shot_bonus,
     '_fullhouse': actual_scores['_fullhouse'] + first_shot_bonus,
     '_four_of_a_kind': actual_scores['_four_of_a_kind'] + first_shot_bonus,
@@ -19,14 +22,7 @@
   };
 
   var _single_score = function(play, val) {
-    if (play == '_4')
-      return 4*val;
-    else if (play == '_5')
-      return 5*val;
-    else if (play == '_6')
-      return 6*val;
-    else
-      return val*actual_scores[play];
+    return val*actual_scores[play];
   };
 
   alseis.Scoresheet = function(no, player) {
@@ -146,7 +142,7 @@
     var _max_score = 0;
     for (var p in this.notes) {
       if (this.notes.hasOwnProperty(p)) {
-        _max_score += this.notes[p] ? this.notes[p] : max_scores[p];
+        _max_score += this.notes[p] != undefined ? this.notes[p] : max_scores[p];
       }
     }
     return _max_score;
